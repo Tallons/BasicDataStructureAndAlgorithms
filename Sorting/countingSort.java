3490313502fc69f04a13681d94d07c6bc379a362
Tallons <Tallons27@yahoo.com>
2020-09-14 00:23:43 -0600

    counting sort commit

--- Sorting/countingSort.java
+++ Sorting/countingSort.java
@@ -3,10 +3,12 @@ import java.util.Arrays;
 public class countingSort{
 
        // Counting Sort Big O  =  O of n - NOT STABLE & you are making assumptions about the data
+       // More efficient with smaller rangers of numbers ( min & max )
 
     public static void main(String[] args) {
-        int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
-        countingSort(intArray, -22, 55);
+               int[] intArray = { 10, 7, 5, 2, 2, 7, 8, 8, 9, 2, 1, 1, 2, 1, 0, 10, 8, 7, 4, 5}; // good data to use counting on
+      //   int[] intArray = { 20, 35, -15, 7, 55, 1, -22};  // not best data to use counting sort
+        countingSort(intArray, 0, 10);
         System.out.println(Arrays.toString(intArray));
     }
 
